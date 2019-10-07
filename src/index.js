7e6439e40ca06c5e9fc1b88aecdf046b1b121445
kmicato <kabanofor@yahoo.com>
2019-10-07 19:05:38 +0300

    made the application 2-in-1 "use both DSC and Developer Student Clubs"

--- src/index.js
+++ src/index.js
@@ -10,8 +10,16 @@ class App extends Component {
     super();
     this.state = {
       scale: 1,
-      name: ""
+      name: "",
+      naming_scheme: ""
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({naming_scheme: event.target.value}, () => {
+                this.drawImage();
+              });
   }
   componentDidMount(){
     WebFont.load({
@@ -42,6 +50,12 @@ class App extends Component {
         </div>
         <p>Start editing to see some magic happen :)</p>
         {this.renderScaleButton()}
+          <br></br>
+        <select defaultValue={'Select Naming Scheme'} onChange={this.handleChange}>
+            <option disabled={true}>Select Naming Scheme</option>
+            <option value="DSC">DSC</option>
+            <option value="Developer Student Clubs">Developer Student Clubs</option>
+        </select>
         <TextField
           label="University"
           margin="normal"
@@ -81,7 +95,7 @@ class App extends Component {
           SAVE IMAGE
         </Button>
         <footer>
-          Made with <span role="img" aria-label="love">❤️ </span> by <a href="https://twitter.com/shanggyilim">@shanggyilim</a>. <a href="https://github.com/shangyilim/dsc-logo-generator">GitHub</a>
+          Made with <span role="img" aria-label="love">❤️ </span> by <a href="https://twitter.com/shanggyilim" target="_blank"  rel="noopener noreferrer">@shanggyilim</a> & <a href="https://twitter.com/kmicato" target="_blank"  rel="noopener noreferrer">@kmicato</a>. <a href="https://github.com/shangyilim/dsc-logo-generator">GitHub</a>
         </footer>
       </div>
     );
@@ -89,14 +103,18 @@ class App extends Component {
 
   drawImage() {
     const name = this.state.name;
+    const naming_scheme = this.state.naming_scheme;
     const scale = this.state.scale;
     const ctx = this.logoCanvas.getContext("2d");
-    ctx.font = `94px "Product Sans"`;
+    ctx.font = `54px "Product Sans"`;
 
-    const canvasWidth = ctx.measureText("DSC " + name).width + this.dscLogo.width + 80;
+    const canvasWidth = ctx.measureText("Developer Student Clubs " + name).width + this.dscLogo.width + 500;
 
     this.logoCanvas.setAttribute("width", canvasWidth * scale);
-    this.logoCanvas.setAttribute("height", this.dscLogo.height * scale);
+    if (naming_scheme === "DSC")
+        this.logoCanvas.setAttribute("height", this.dscLogo.height * scale);
+    else
+        this.logoCanvas.setAttribute("height", (this.dscLogo.height * 2 * scale) - 40);
 
     ctx.scale(scale, scale);
     ctx.font = `94px "Product Sans"`;
@@ -104,7 +122,13 @@ class App extends Component {
 
     ctx.drawImage(this.dscLogo, 20, 0, this.dscLogo.width, this.dscLogo.height);
 
-    ctx.fillText("DSC " + name, this.dscLogo.width + 40, 110);
+    //if DSC is selected, draw on one line
+    if(naming_scheme === "DSC") {
+        ctx.fillText(naming_scheme + " " + name, this.dscLogo.width + 40, 110);
+    } else {
+        ctx.fillText(naming_scheme, this.dscLogo.width + 40, 110);
+        ctx.fillText(name, this.dscLogo.width + 40, 200);
+    }
 
     this.setState({
       fullLogoUrl: this.logoCanvas.toDataURL()

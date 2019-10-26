0614aa57bed18e0204f5e6f5e0a96039d34df413
kmicato <kabanofor@yahoo.com>
2019-10-26 18:44:12 +0300

    added vertical view

--- src/index.js
+++ src/index.js
@@ -8,16 +8,24 @@ import "./style.css";
 class App extends Component {
   constructor() {
     super();
+    this.ref = React.createRef();
     this.state = {
       scale: 1,
       name: "",
-      naming_scheme: ""
+      naming_scheme: "",
+      structure: ""
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleStructureChange = this.handleStructureChange.bind(this);
   }
   handleChange(event) {
     this.setState({naming_scheme: event.target.value}, () => {this.drawImage();});
+  }
+
+  handleStructureChange(event) {
+
+    this.setState({structure: event.target.value}, () => {this.drawImage();});
   }
   componentDidMount(){
     WebFont.load({
@@ -50,16 +58,16 @@ class App extends Component {
             <p>Start editing to see some magic happen :)</p>
             {this.renderScaleButton()}
             <br></br>
-            <select defaultValue={'Select Naming Scheme'} onChange={this.handleChange}>
-                <option disabled={true}>Select Naming Scheme</option>
-                <option value="DSC">DSC</option>
-                <option value="Developer Student Clubs">Developer Student Clubs</option>
-            </select>
-            <br></br>
-            <select defaultValue={'Select Style'} onChange={this.handleChange}>
+            <select defaultValue={'Select Style'} onChange={this.handleStructureChange}>
                 <option disabled={true}>Select Style</option>
                 <option value="logo_right">Logo Right</option>
                 <option value="logo_center">Logo Center</option>
+            </select>
+            <br></br>
+            <select defaultValue={'Select Naming Scheme'} ref="naming_scheme" onChange={this.handleChange}>
+                <option disabled={true}>Select Naming Scheme</option>
+                <option value="DSC">DSC</option>
+                <option value="Developer Student Clubs">Developer Student Clubs</option>
             </select>
             <br></br>
             <TextField
@@ -100,42 +108,51 @@ class App extends Component {
             >
               SAVE IMAGE
             </Button>
-            <footer>
+          </div>
+          <footer>
               Made with <span role="img" aria-label="love">❤️ </span> by <a href="https://twitter.com/shanggyilim" target="_blank"  rel="noopener noreferrer">@shanggyilim</a> & <a href="https://twitter.com/kmicato" target="_blank"  rel="noopener noreferrer">@kmicato</a>. <a href="https://github.com/shangyilim/dsc-logo-generator">GitHub</a>
             </footer>
-          </div>
       </div>
     );
   }
 
   drawImage() {
     const name = this.state.name;
+    const structure = this.state.structure;
     const naming_scheme = this.state.naming_scheme;
     const scale = this.state.scale;
     const ctx = this.logoCanvas.getContext("2d");
     ctx.font = `54px "Product Sans"`;
 
     const canvasWidth = ctx.measureText("Developer Student Clubs " + name).width + this.dscLogo.width + 500;
-
     this.logoCanvas.setAttribute("width", canvasWidth * scale);
+
     if (naming_scheme === "DSC")
         this.logoCanvas.setAttribute("height", this.dscLogo.height * scale);
     else
-        this.logoCanvas.setAttribute("height", (this.dscLogo.height * 2 * scale) - 40);
+        this.logoCanvas.setAttribute("height", (this.dscLogo.height * 2 * scale) + 50);
 
     ctx.scale(scale, scale);
     ctx.font = `94px "Product Sans"`;
     ctx.fillStyle = "rgba(0, 0, 0, 0.54)";
+    let imageDX = (structure === "logo_right" || structure === "")? 0 : (ctx.measureText("Developer Student Clubs ").width + this.dscLogo.width)/2;
+    ctx.drawImage(this.dscLogo, imageDX, 0, this.dscLogo.width, this.dscLogo.height);
 
-    ctx.drawImage(this.dscLogo, 20, 0, this.dscLogo.width, this.dscLogo.height);
+    //if logo center
+      if(structure === "logo_center") {
+            ctx.fillText(naming_scheme, this.dscLogo.width + 40, this.dscLogo.height + 50);
+            ctx.fillText(name, this.dscLogo.width + 40, (this.dscLogo.height) * 2);
+            // this.ref.naming_scheme
+      } else {
+           //if DSC is selected, draw on one line
+            if(naming_scheme === "DSC") {
+                ctx.fillText(naming_scheme + " " + name, this.dscLogo.width + 40, 110);
+            } else {
+                ctx.fillText(naming_scheme, this.dscLogo.width + 40, 110);
+                ctx.fillText(name, this.dscLogo.width + 40, 200);
+            }
+      }
 
-    //if DSC is selected, draw on one line
-    if(naming_scheme === "DSC") {
-        ctx.fillText(naming_scheme + " " + name, this.dscLogo.width + 40, 110);
-    } else {
-        ctx.fillText(naming_scheme, this.dscLogo.width + 40, 110);
-        ctx.fillText(name, this.dscLogo.width + 40, 200);
-    }
 
     this.setState({
       fullLogoUrl: this.logoCanvas.toDataURL()

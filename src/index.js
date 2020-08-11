331c966b3f47e34a9865156c0e19925f8259f7c5
stybl <sty@stybl.net>
2020-08-11 10:55:51 +0300

    Solved merge conflict

--- src/index.js
+++ src/index.js
@@ -8,20 +8,26 @@ import "./style.css";
 class App extends Component {
   constructor() {
     super();
+    this.ref = React.createRef();
     this.state = {
       scale: 1,
       name: "",
       naming_scheme: "",
+      structure: "",
       colorful: true
     };
 
-    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleStructureChange = this.handleStructureChange.bind(this);
     this.handleColorChange = this.handleColorChange.bind(this);
   }
-  handleNameChange(event) {
-    this.setState({naming_scheme: event.target.value}, () => {
-                this.drawImage();
-              });
+  handleChange(event) {
+    this.setState({naming_scheme: event.target.value}, () => {this.drawImage();});
+  }
+
+  handleStructureChange(event) {
+
+    this.setState({structure: event.target.value}, () => {this.drawImage();});
   }
   handleColorChange(event){
     this.setState({colorful: event.target.value === "Colorful"}, () => {
@@ -42,80 +48,88 @@ class App extends Component {
   render() {
     return (
       <div className="main">
-        <h1>DSC Lockup Generator</h1>
-        <div style={hidden}>
-          <img
-            ref={e => {
-              this.dscLogo = e;
-            }}
-            onLoad={() => {
-              this.drawImage();
-            }}
-            src= {this.state.colorful ? "dsc_icon-01.svg" : "dsc_icon-02.svg"}
-            alt={`DSC Icon`}
-          />
-        </div>
-        <p>Start editing to see some magic happen :)</p>
-        <div className="controls">
-        {this.renderScaleButton()}
-        <select defaultValue={'Select Naming Scheme'} onChange={this.handleNameChange}>
-            <option disabled={true}>Select Naming Scheme</option>
-            <option value="DSC">DSC</option>
-            <option value="Developer Student Clubs">Developer Student Clubs</option>
-        </select>
-        <select defaultValue={'Select Coloration'} onChange={this.handleColorChange}>
+          <div>
+            <h1>DSC Lockup Generator</h1>
+            <div style={hidden}>
+              <img
+                ref={e => {
+                  this.dscLogo = e;
+                }}
+                onLoad={() => {
+                  this.drawImage();
+                }}
+                src={this.state.colorful ? "dsc_icon-01.svg" : "dsc_icon-02.svg"}
+                alt={`DSC Icon`}
+              />
+            </div>
+            <p>Start editing to see some magic happen :)</p>
+            <div className="controls">
+              {this.renderScaleButton()}
+              <select defaultValue={'Select Style'} onChange={this.handleStructureChange}>
+                  <option disabled={true}>Select Style</option>
+                  <option value="logo_right">Logo Right</option>
+                  <option value="logo_center">Logo Center</option>
+              </select>
+              <select defaultValue={'Select Naming Scheme'} ref="naming_scheme" onChange={this.handleChange}>
+                  <option disabled={true}>Select Naming Scheme</option>
+                  <option value="DSC">DSC</option>
+                  <option value="Developer Student Clubs">Developer Student Clubs</option>
+              </select>
+              <select defaultValue={'Select Coloration'} onChange={this.handleColorChange}>
             <option disabled={true}>Select Coloration</option>
             <option value="Colorful">Colorful</option>
             <option value="Knockout">Knockout</option>
         </select>
-        </div>
-        <TextField
-          label="University"
-          margin="normal"
-          onChange={e => {
-            this.setState(
-              {
-                name: e.target.value
-              },
-              () => {
-                this.drawImage();
-              }
-            );
-          }}
-        />
-        <br />
-        <canvas
-          style={hidden}
-          ref={e => {
-            this.logoCanvas = e;
-          }}
-        ></canvas>
+            </div>
+            <TextField
+              label="University"
+              margin="normal"
+              onChange={e => {
+                this.setState(
+                  {
+                    name: e.target.value
+                  },
+                  () => {
+                    this.drawImage();
+                  }
+                );
+              }}
+            />
+            <br />
+            <canvas
+              style={hidden}
+              ref={e => {
+                this.logoCanvas = e;
+              }}
+            ></canvas>
         <div className={this.state.colorful ? "full-logo-container" : "full-logo-container dark"}>
-          <img
-            ref={e => {
-              this.fullLogoImg = e;
-            }}
-            alt={`DSC ${this.state.name} Logo`}
-            src={this.state.fullLogoUrl}
-          />
-        </div>
-        <Button
-          variant="contained"
-          color="primary"
-          href={this.state.fullLogoUrl}
-          download={`DSC ${this.state.name} Logo x${this.state.scale}.png`}
-        >
-          SAVE IMAGE
-        </Button>
-        <footer>
+              <img
+                ref={e => {
+                  this.fullLogoImg = e;
+                }}
+                alt={`DSC ${this.state.name} Logo`}
+                src={this.state.fullLogoUrl}
+              />
+            </div>
+            <Button
+              variant="contained"
+              color="primary"
+              href={this.state.fullLogoUrl}
+              download={`DSC ${this.state.name} Logo x${this.state.scale}.png`}
+            >
+              SAVE IMAGE
+            </Button>
+          </div>
+          <footer>
           Made with <span role="img" aria-label="love">❤️ </span> by <a href="https://twitter.com/shanggyilim" target="_blank"  rel="noopener noreferrer">@shanggyilim</a>, <a href="https://twitter.com/kmicato" target="_blank"  rel="noopener noreferrer">@kmicato</a> & <a href="https://github.com/stelioslogothetis" target="_blank"  rel="noopener noreferrer">@stelioslogothetis</a>. <a href="https://github.com/shangyilim/dsc-logo-generator">GitHub</a>
-        </footer>
+            </footer>
       </div>
     );
   }
 
   drawImage() {
     const name = this.state.name;
+    const structure = this.state.structure;
     const naming_scheme = this.state.naming_scheme;
     const colorful = this.state.colorful;
     const scale = this.state.scale;
@@ -123,26 +137,34 @@ class App extends Component {
     ctx.font = `54px "Product Sans"`;
 
     const canvasWidth = ctx.measureText("Developer Student Clubs " + name).width + this.dscLogo.width + 500;
-
     this.logoCanvas.setAttribute("width", canvasWidth * scale);
+
     if (naming_scheme === "DSC")
         this.logoCanvas.setAttribute("height", this.dscLogo.height * scale);
     else
-        this.logoCanvas.setAttribute("height", (this.dscLogo.height * 2 * scale) - 40);
+        this.logoCanvas.setAttribute("height", (this.dscLogo.height * 2 * scale) + 50);
 
     ctx.scale(scale, scale);
     ctx.font = `94px "Product Sans"`;
     ctx.fillStyle = colorful ? "rgb(103, 108, 114)" : "rgb(255, 255, 255)";
+    let imageDX = (structure === "logo_right" || structure === "")? 0 : (ctx.measureText("Developer Student Clubs ").width + this.dscLogo.width)/2;
+    ctx.drawImage(this.dscLogo, imageDX, 0, this.dscLogo.width, this.dscLogo.height);
 
-    ctx.drawImage(this.dscLogo, 20, 0, this.dscLogo.width, this.dscLogo.height);
+    //if logo center
+      if(structure === "logo_center") {
+            ctx.fillText(naming_scheme, this.dscLogo.width + 40, this.dscLogo.height + 50);
+            ctx.fillText(name, this.dscLogo.width + 40, (this.dscLogo.height) * 2);
+            // this.ref.naming_scheme
+      } else {
+           //if DSC is selected, draw on one line
+            if(naming_scheme === "DSC") {
+                ctx.fillText(naming_scheme + " " + name, this.dscLogo.width + 40, 110);
+            } else {
+                ctx.fillText(naming_scheme, this.dscLogo.width + 40, 110);
+                ctx.fillText(name, this.dscLogo.width + 40, 200);
+            }
+      }
 
-    //if DSC is selected, draw on one line
-    if(naming_scheme === "DSC") {
-        ctx.fillText(naming_scheme + " " + name, this.dscLogo.width + 40, 110);
-    } else {
-        ctx.fillText(naming_scheme, this.dscLogo.width + 40, 110);
-        ctx.fillText(name, this.dscLogo.width + 40, 200);
-    }
 
     this.setState({
       fullLogoUrl: this.logoCanvas.toDataURL()
